11d1517bb3a31638abd09151cff6502a5c739d12
hnong1 <Huy.Nong.219@gmail.com>
2026-04-23 00:54:04 -0500

    Charts Added

--- src/main/java/com/group37/sentencebuilder/ui_layer/model/FileCorpusStatRow.java
+++ src/main/java/com/group37/sentencebuilder/ui_layer/model/FileCorpusStatRow.java
@@ -9,12 +9,22 @@ public class FileCorpusStatRow {
     private final StringProperty fileName;
     private final StringProperty sentences;
     private final StringProperty words;
+    private final StringProperty uniqueWords;
+    private final StringProperty topPairCount;
     private final StringProperty imported;
 
-    public FileCorpusStatRow(String fileName, String sentences, String words, String imported) {
+    public FileCorpusStatRow(
+            String fileName,
+            String sentences,
+            String words,
+            String uniqueWords,
+            String topPairCount,
+            String imported) {
         this.fileName = new SimpleStringProperty(fileName);
         this.sentences = new SimpleStringProperty(sentences);
         this.words = new SimpleStringProperty(words);
+        this.uniqueWords = new SimpleStringProperty(uniqueWords);
+        this.topPairCount = new SimpleStringProperty(topPairCount);
         this.imported = new SimpleStringProperty(imported);
     }
 
@@ -28,6 +38,14 @@ public class FileCorpusStatRow {
 
     public StringProperty wordsProperty() {
         return words;
+    }
+
+    public StringProperty uniqueWordsProperty() {
+        return uniqueWords;
+    }
+
+    public StringProperty topPairCountProperty() {
+        return topPairCount;
     }
 
     public StringProperty importedProperty() {
