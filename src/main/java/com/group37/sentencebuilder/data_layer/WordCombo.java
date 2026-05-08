dfc68ee27e931c350dc8e9a7030512c267eb75d0
CortKimzey <143350606+CortKimzey@users.noreply.github.com>
2026-05-07 19:06:23 -0500

    Update WordCombo Object
    Updated description of the WordCombo object

--- src/main/java/com/group37/sentencebuilder/data_layer/WordCombo.java
+++ src/main/java/com/group37/sentencebuilder/data_layer/WordCombo.java
@@ -5,15 +5,16 @@
  *  Author:  Cortland Kimzey
  *
  *  Description:
- *      Object to hold the data of a word combo object.
+ *      Object to hold the data of a word combo from the database.
  *
- *  Version: 1.0
+ * Version: 1.0
  * Created: 2026-03-25
- * Last Modified: 2026-03-25
+ * Last Modified: 2026-05-07
  *
  *  Responsibilities:
- *      - <responsibilities 1>
- *      - <responsibilities 2>
+ *      - Store the first word of the combination as a word object
+ *      - Store the second word of the combination as a word object
+ *      - Stores the number of times the combination has been seen from the database
  * ------------------------------------------------------------
  */
 
@@ -27,6 +28,16 @@ public class WordCombo
     private Word nextWord;
     private int comboCount;
 
+    /**
+     * Author: Cortland Kimzey
+     * Description: 
+     *      A constructor to create the WordCombo object with all info stored.
+     *      Stores the firstWord, nextWord, and comboCount from the database.
+     * 
+     * @param firstWord the first word word object of the combination
+     * @param nextWord the second word word object of the combination
+     * @param comboCount the number of times this combo has been seen
+     */
     public WordCombo(Word firstWord, Word nextWord, int comboCount)
     {
         this.firstWord = firstWord;
@@ -34,16 +45,37 @@ public class WordCombo
         this.comboCount = comboCount;
     }
 
+    /**
+     * Author: Cortland Kimzey
+     * Description: 
+     *      Gets the comboCount from the object
+     * 
+     * @return the comboCount of the combination
+     */
     public int getComboCount()
     {
         return comboCount;
     }
 
+    /**
+     * Author: Cortland Kimzey
+     * Description: 
+     *      Gets the nextWord object from this object
+     * 
+     * @return the nextWord word object
+     */
     public Word getNext()
     {
         return nextWord;
     }
 
+    /**
+     * Author: Cortland Kimzey
+     * Description: 
+     *      Gets the ID of the next word from the nextWord word object
+     * 
+     * @return the word ID of the nextWord word object
+     */
     public int getNextID()
     {
         return nextWord.getWordID();
